3d7f0233296d014195e0ab21c801747d4e450f63
ElenaGrigoruta <elena.grigoruta@gmail.com>
2023-09-22 02:52:50 +0300

    3rd commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -67,4 +67,6 @@ dependencies {
     implementation("androidx.compose.material:material:1.0.0")
     implementation("androidx.activity:activity-compose:1.3.1")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:1.0.0-alpha07")
+    implementation("io.coil-kt:coil-compose:1.4.0")
+
 }
--- app/src/main/java/com/elenagrigoruta/movieapp/widgets/widgets.kt
+++ app/src/main/java/com/elenagrigoruta/movieapp/widgets/widgets.kt
@@ -1,7 +1,9 @@
 package com.elenagrigoruta.movieapp.widgets
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -11,6 +13,7 @@ import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
 import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
@@ -19,11 +22,20 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import coil.compose.rememberImagePainter
+import coil.transform.CircleCropTransformation
 import com.elenagrigoruta.movieapp.model.Movie
+import com.elenagrigoruta.movieapp.model.getMovies
 
+
+@Preview
 @Composable
-fun MovieRow(movie: Movie, onItemClick: (String) -> Unit = {}) {
+fun MovieRow(
+    movie: Movie = getMovies()[1],
+    onItemClick: (String) -> Unit = {}
+) {
     Card(
         modifier = Modifier
             .padding(4.dp)
@@ -47,12 +59,36 @@ fun MovieRow(movie: Movie, onItemClick: (String) -> Unit = {}) {
                 elevation = 4.dp
             )
             {
-                Icon(
+
+                Image(painter = rememberImagePainter(data = movie.images[0],
+                    builder = {
+                        crossfade(true)
+                        transformations(CircleCropTransformation())
+                    }),
+                    contentDescription = "Movie Poster")
+
+                /*Icon(
                     imageVector = Icons.Default.AccountBox,
                     contentDescription = "Movie Image"
-                )
+                )*/
             }
-            Text(text = movie.title)
+
+            Column(modifier = Modifier.padding(4.dp)) {
+                Text(
+                    text = movie.title,
+                    style = MaterialTheme.typography.h6
+                )
+                Text(
+                    text = "Director: ${movie.director}",
+                    style = MaterialTheme.typography.caption
+                )
+                Text(
+                    text = "Released: ${movie.year}",
+                    style = MaterialTheme.typography.caption
+                )
+
+            }
+
 
         }
     }
